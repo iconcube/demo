bb28258b553ec6958f98327ea08022cc784de0ac
iconcube <wooky1188@gmail.com>
2026-03-16 16:35:27 +0900

    demo_test

---
+++ scripts/install_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Amazon Linux에 Apache 웹 서버 설치
+yum update -y
+yum install -y httpd
---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Apache 웹 서버 시작 및 자동 실행 등록
+systemctl start httpd
+systemctl enable httpd
